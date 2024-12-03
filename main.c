9a0452d5d8fe93ca08c4181a724f4cac5b573b7f
David Schwartzberg <dschwartzberg50@stuy.edu>
2024-12-02 23:13:25 -0500

    very simple io

--- main.c
+++ main.c
@@ -3,10 +3,18 @@
 #include <string.h>
 #include <unistd.h>
 #include <errno.h>
+#include <fcntl.h>
+
 #include "functions.h"
 
+#define LINE_BUFFER_SIZE 256
+
 int main(int argc, char* argv[]) {
-  printf("test: %d\n", func(10));
+	printf("Enter command: ");
+	fflush(stdin);
+	char line_buffer[LINE_BUFFER_SIZE];
+	fgets(line_buffer, LINE_BUFFER_SIZE-1, stdin);
+	printf("string you entered (parse later): %s\n", line_buffer);
 
 	return 0;
 }
