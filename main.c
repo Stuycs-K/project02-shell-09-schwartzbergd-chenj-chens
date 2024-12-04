e6220c62e6d4438e603837524325ecfe4cbba6d4
STACEY_CHEN <schen50@stuy.edu>
2024-12-04 14:00:48 -0500

    tested redirstdin

--- main.c
+++ main.c
@@ -17,6 +17,8 @@ int main(int argc, char* argv[]) {
 	fgets(line_buffer, LINE_BUFFER_SIZE-1, stdin);
 	redir("foo.txt");
 	printf("string you entered (parse later): %s\n", line_buffer);
+	fgets(line_buffer, LINE_BUFFER_SIZE-1, stdin);
+	printf("string 2 you entered (parse later): %s\n", line_buffer);
 
 
 	return 0;
