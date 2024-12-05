b22ea55c9b810737b7b29d51f751944a5771a86b
Jialan <jialanc@nycstudents.net>
2024-12-04 21:58:46 -0500

    minor changes to exiting

--- functions.c
+++ functions.c
@@ -5,9 +5,20 @@
 #include <errno.h>
 #include <fcntl.h>
 
+char * getdir() {
+  char * cwd;
+  int s = sizeof(char) * 1024;
+  cwd = (char *)malloc(s);
+  if (getcwd(cwd, s)==NULL){
+    printf("Error: %s\n", strerror(errno));
+  }
+  return cwd;
+}
+
+
 char* get_input() {
   int BUFFER_SIZE = 256;
-  printf("Enter command: ");
+  printf("%s $ ",getdir());
   fflush(stdin);
   char* line_buffer = (char*) malloc(BUFFER_SIZE * sizeof(char));
   fgets(line_buffer, BUFFER_SIZE-1, stdin); line_buffer[BUFFER_SIZE-1] = '\0';
@@ -29,14 +40,3 @@ split: takes a string and splits over the delimiters, returns the array of the s
 //
 //	return arg_array;
 //}
-
-// doesn't shorten with ~ yet
-char * getdir() {
-  char * cwd;
-  int s = sizeof(char) * 1024;
-  cwd = (char *)malloc(s);
-  if (getcwd(cwd, s)==NULL){
-    printf("Error: %s\n", strerror(errno));
-  }
-  return cwd;
-}
