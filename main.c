9b181289caafc9885db1a2b59fe9a215aebca227
David Schwartzberg <dschwartzberg50@stuy.edu>
2024-12-05 21:28:29 -0500

    fixed indentation and added extra error message if execvp fails

--- main.c
+++ main.c
@@ -18,58 +18,56 @@ void printArr(char** arr) {
 }
 
 int main(int argc, char* argv[]) {
-	int forkpid;
+	char* cwd;
 	char* input;
 	char** cmd_array;
 	char** arg_array;
+	int forkpid;
 
 	int iters = 0;
-//	while (1) {
-            
-        //printf("HOME: %s\n", getenv("HOME"));
-		//printf("iters: %d, command line! enter command: ", iters); // remove this later to be just $
-		//fflush(stdout);
-		//input = get_input(); // will wait until enter
-		while (1) {
-            printf("%s $ ",getdir());
-            input=get_input();
-			if (strcmp(input, "exit")==0) {
-                exit(1);
-            }
-		    cmd_array = split(input, ";"); // first split over the semicolons
-		    int i = 0;
-		    do {
-			    // printf("arg %d: '%s'\n", i, cmd_array[i]);
-			    arg_array = split(cmd_array[i], " "); // then split over spaces
-			    printArr(arg_array);
-
-			    forkpid = fork();
-			    if (forkpid == -1) {
-				    perror("Failed to fork");
-				    return -1;
-			    } else if (forkpid == 0) {
-				    printf("child here %d!\n", getpid());
-				    execvp(arg_array[0], arg_array);
-
-				    return 0;
-			    } else {
-				    printf("parent here %d!\n", getpid());
-				    printf("child pid: %d\n", forkpid);
-
-				    int status;
-				    waitpid(forkpid, &status, 0);
-				    printf("child exited\n");
-			    }
-
-			    free(arg_array);
-			    printf("\n");
-			    i++;
-		    } while(cmd_array[i]!=NULL);
-
-		    free(input);
-
-		    iters++;
+	while (1) {
+		cwd = getdir(); // needs to be changed for "cd" commands
+		printf("%s$ ", cwd);
+		
+    input = get_input();
+		if (strcmp(input, "exit") == 0) {
+			printf("exiting...\n"); // remove in final, keep for testing maybe
+			exit(0);
     }
+		
+    cmd_array = split(input, ";"); // split over the semicolons
+		for (int i = 0; cmd_array[i]!=NULL; i++) {
+			// printf("arg %d: '%s'\n", i, cmd_array[i]);
+	    arg_array = split(cmd_array[i], " "); // then split over spaces
+	    printArr(arg_array);
+
+	    forkpid = fork();
+	    if (forkpid == -1) {
+		    perror("Failed to fork");
+		    return -1;
+	    } else if (forkpid == 0) {
+		    execvp(arg_array[0], arg_array);
+					
+				// only reaches here if execvp fails
+				char error_string[BUFFER_SIZE];
+				sprintf(error_string, "%s", arg_array[0]);
+				perror(error_string);
+				
+				return 1;
+	    } else {
+		    int status;
+		    waitpid(forkpid, &status, 0);
+		    printf("child exited\n");
+	    }
+
+	    free(arg_array);
+	    printf("\n");
+		}
+
+    free(input);
+
+		iters++; // not needed technically
+  }
 	printf("how did you get here?\n");
 	return 1;
 }
