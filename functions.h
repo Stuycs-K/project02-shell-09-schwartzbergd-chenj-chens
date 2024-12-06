9ff2f9c5cf9fd27fcb64d33cfc7a1f92a99000db
David Schwartzberg <dschwartzberg50@stuy.edu>
2024-12-05 22:11:43 -0500

    removed err() function, instead give message to perror()

--- functions.h
+++ functions.h
@@ -8,7 +8,6 @@ char * getdir();
 char * get_input();
 char ** split(char* string, char* delimiters);
 
-int err();
 void redirstdout(char * fileName);
 void redirstdin(char * fileName);
 void redir(char** arr);
