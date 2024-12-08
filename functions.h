7d937123b9e5d3b7e05806a2a58205bc8628c92e
Jialan <jialanc@nycstudents.net>
2024-12-08 13:26:52 -0500

    refactor code

--- functions.h
+++ functions.h
@@ -1,9 +1,24 @@
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
 
-// test
+#define BUFFER_SIZE 256
+#define ARRAY_SIZE 16
+
 char * getdir();
+char * shortdir();
+void print_dir();
+void cd_check(char** arg_array);
 char * get_input();
-//char ** split(char* string, char* delimiters);
+char ** split(char* string, char* delimiters);
+
+char** get_command(char* input, char** cmd_array);
+void child_process(int forkpid2, int pipeIndex, char** arg_array);
+void grandchild_process(int pipeIndex, char** arg_array);
+void delete_temp(char * file);
+void redirstdout(char * fileName);
+void redirstdin(char * fileName);
+void redir(char** arr);
+int checkforpipe(char ** arr);
+
 
 #endif
--- main.c
+++ main.c
@@ -4,18 +4,84 @@
 #include <unistd.h>
 #include <errno.h>
 #include <fcntl.h>
+#include <sys/wait.h>
 
 #include "functions.h"
 
-#define LINE_BUFFER_SIZE 256
+#define EOF_CHAR 0x04
 
- int main(int argc, char* argv[]) {
-    char * n;
-    printf("HOME: %s\n", getenv("HOME"));
-    while(n=get_input()) {
-        if (strcmp(n, "exit\n")==0 || strcmp(n, "\n")==0) {
-            exit(1);
+// just for testing, no real functionality
+void printArr(char** arr) {
+	int j = 0;
+	do {
+		printf("arg %d: '%s'\n", j, arr[j]);
+		j++;
+	} while(arr[j]!=NULL);
+}
+
+int main(int argc, char* argv[]) {
+	// int backup_stdout = dup(STDOUT_FILENO);
+	// int backup_stdin = dup(STDIN_FILENO);
+	// char* cwd;
+	char* input;
+	char** cmd_array;
+	char** arg_array;
+	char error_string[BUFFER_SIZE];
+
+	while (1) {
+        print_dir();
+
+        input = get_input();
+		if (input == NULL) { //from file
+			printf("EOF, exiting\n");
+			return 0;
+		}
+        arg_array = get_command(input, cmd_array);
+        if (!arg_array) {
+            return 0;
         }
-    }
- 	return 0;
- }
+
+		// cd check
+        if (strcmp(arg_array[0], "cd") == 0) {
+            cd_check(arg_array);
+            continue; // required to make sure cd isn't tried to be execvp()ed
+        }
+
+        int forkpid = fork();
+        int forkpid2;
+	    if (forkpid == -1) {
+		    perror("Failed to fork");
+		    return 1;
+	    } else if (forkpid == 0) {
+
+        int pipeIndex = checkforpipe(arg_array);
+        if (pipeIndex !=-1){ // if pipe exists
+            forkpid2 = fork(); // then fork
+            if (forkpid2 == -1){
+                perror("Failed to fork");
+                return 1;
+            } else if (forkpid2 == 0){ // grandchild process
+                grandchild_process(pipeIndex, arg_array);
+            } else{ // child process
+                child_process(forkpid2, pipeIndex, arg_array);
+            }
+        }
+        redir(arg_array);
+		execvp(arg_array[0], arg_array);
+
+        // only reaches here if execvp fails
+        sprintf(error_string, "%s", arg_array[0]);
+        perror(error_string);
+		return 1;
+        
+	    } else {
+		    int status;
+		    waitpid(forkpid, &status, 0);
+	    }
+	    free(arg_array);
+		free(input);
+  }
+
+	printf("??? You shouldn't be here, probably\n");
+	return 1;
+}
